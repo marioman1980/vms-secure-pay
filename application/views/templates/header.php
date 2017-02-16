c2938bd7336aaf49f1b452d586ed98b15ce67205
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-16 10:14:14 +0000

    Initial Commit
    Includes CodeIgniter, w3css, header template and User main view

---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE>
+<html style="min-height:100%">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+  	<title>VMS Secure Payments</title> 
+	<link rel="icon" href="images/logo.ico">
+	<link rel="stylesheet" type="text/css" href="css/w3css.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+	<div id="container" class="w3-container">
+		<header class="w3-container w3-padding-8">
+			<div id="header-img"><img id="logo" src="images/vms_secure_header.jpg"><br>
+<!--			   <span style="float:right;padding-top:2px">Powered by Stripe</span>-->
+		   </div>	
+			<br><hr>
+		</header>	
