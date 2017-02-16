24845f1ece808952bfb8ce31dc0ab13b6cdae05e
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-16 10:53:04 +0000

    Add watermark
    Test site watermark added for dev purposes

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -16,9 +16,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
 	<div id="container" class="w3-container">
+		<div id="watermark">TEST SITE</div><!--REMOVE POST-DEVELOPMENT-->
 		<header class="w3-container w3-padding-8">
 			<div id="header-img"><img id="logo" src="images/vms_secure_header.jpg"><br>
 <!--			   <span style="float:right;padding-top:2px">Powered by Stripe</span>-->
 		   </div>	
+			SITE UNDER DEVELOPMENT<br>TEST PURPOSES ONLY<!--REMOVE POST-DEVELOPMENT-->
 			<br><hr>
 		</header>	
