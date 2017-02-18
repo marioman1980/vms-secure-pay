0b6d3636aafe15e0b7387b1285bae5db9d24d3ac
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-18 20:46:00 +0000

    Absolute link works in Outlook
    Tried an absolute link - now works in Outlook

--- application/views/email.php
+++ application/views/email.php
@@ -226,7 +226,7 @@ span{
 										<td class="btn-primary" bgcolor="#3498DB" align="center" style="border-radius:4px;" width="250" height="50">
 											<div class="contentEditableContainer contentTextEditable" >
                           <div class="contentEditable" style='text-align:center;'>
-                              <a target='_blank' href="#" style="color:#fff;text-decoration:none;font-family:Helvetica, Arial, sans-serif;font-size:16px;color:#fff;border-radius:4px;">Complete Payment</a>
+                              <a target='_blank' href="https://www.jkamradcliffe.net" style="color:#fff;text-decoration:none;font-family:Helvetica, Arial, sans-serif;font-size:16px;color:#fff;border-radius:4px;">Complete Payment</a>
                           </div>
                       </div>
 										</td>
