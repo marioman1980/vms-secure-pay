356cd09a689ac3989df0749ddadc66a15fac3bb8
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-22 12:14:59 +0000

    Fix issue with sticky footer
    Added closing div to footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,5 @@
-	<footer class="w3-container w3-padding-8" style="height:150px">
+</div>
+  <footer class="w3-container w3-padding-8" style="height:150px">
 		<hr>
 		<span>Footer Information</span><br>
     <a href="mailto:james@jkamradcliffe.net">Contact</a><!-- Temporary as only useful for email clients - not web mail -->
