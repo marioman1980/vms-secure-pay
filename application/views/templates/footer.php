134b8e6038d434874e8db5e0229855185292947e
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-22 07:45:44 +0000

    Add contact email

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,7 @@
 	<footer class="w3-container w3-padding-8" style="height:150px">
 		<hr>
-		<span>Footer Information</span>
+		<span>Footer Information</span><br>
+    <a href="mailto:james@jkamradcliffe.net">Contact</a><!-- Temporary as only useful for email clients - not web mail -->
 		
 	</footer>	
 </body>
