536dde6a7d2d896b5d05c10af584fbd8e5c46c7c
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-23 17:48:46 +0000

    Add content

--- application/views/privacy_policy.php
+++ application/views/privacy_policy.php
@@ -1,3 +1,58 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-My kickass privacy policy
+<div id="privacy-policy" class="w3-container w3-padding-8">
+  <h1>Privacy policy</h1>
+  <p>We collect certain information or data about you when you use VMS Secure Payments.</p>
+  <p>This includes:</p>
+  <ul>
+    <li>contact details (including your email address) for future correspondence</li>
+    <li>information on how you use the site, using client storage to help us improve the website and personalise your experience</li>
+  </ul>
+  <p>This helps us to:</p>
+  <ul>
+    <li>improve the site by monitoring how you use it</li>
+    <li>respond to any communication from you</li>
+  </ul>
+
+<!--
+  <h2>Where your data is stored</h2>
+  <p>We store your data on our secure servers in the UK.</p>
+
+  <p>It may also be stored outside of Europe, where it could be viewed by our staff or suppliers.</p>
+-->
+
+  <p>By submitting your personal data, you agree to this.</p>
+
+  <h2>Keeping your data secure</h2>
+  <p>Transmitting information over the internet is generally not completely secure, and we can’t guarantee the security of your data.</p>
+
+  <strong>Any data you transmit is at your own risk.</strong>
+
+  <p>We have procedures and security features in place to try and keep your data secure once we receive it.</p>
+
+  <p>We won’t share your information with any other organisations for marketing, market research or commercial purposes, and we don’t pass on your details to other websites.</p>
+
+<!--  <strong>Payment transactions are always encrypted.</strong>-->
+
+  <h2>Disclosing your information</h2>
+  <p>We may pass on your personal information if we have a legal obligation to do so. This includes exchanging information with other government departments for legal reasons.</p>
+
+  <h2>Your rights</h2>
+  <p>You can find out what information we hold about you, and ask us not to use any of the information we collect.</p>
+
+  <h2>Links to other websites</h2>
+  <p>VMS Secure Payments contains links to and from other websites.</p>
+
+  <p>This privacy policy only applies to this website.</p>
+
+  <strong>Following a link to another website</strong>
+
+  <p>If you go to another website from this one, read the privacy policy on that website to find out what it does with your information.</p>
+
+  <strong>Following a link to VMS Secure Payments from another website</strong>
+
+  <p>If you come to VMS Secure Payments from another website, we may receive personal information about you from the other website. You should read the privacy policy of the website you came from to find out more about this.</p>
+
+  <p style="color: #708090">Last updated: 21 February 2017</p>
+  <p style="color: #708090">This privacy policy has been adapted from <a href="https://www.gov.uk/help/privacy-policy">www.gov.uk/help/privacy-policy</a></p>
+</div>
