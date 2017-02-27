0aed0a4e66858328f3cf0f6d4bee81030b8f551c
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2017-02-27 08:50:57 +0000

    Adjust to match Stripe Elements

--- application/views/payment_form.php
+++ application/views/payment_form.php
@@ -11,28 +11,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   Full validation/sanitization will be performed server-side
 
   Readonly fields will be pre-populated
--->      
-    <label class="w3-third">Title</label>
-    <input class="w3-twothird input_margin form_readonly" id="title" name="title" type="text" value="<?php echo $title; ?>" style="width:50px" required readonly></input><br><br>
-    <label class="w3-third">First name</label>
-    <input class="w3-twothird field input_margin form_readonly" id="first_name" name="first_name" type="text" value="<?php echo $first_name; ?>" required readonly></input><br><br>
-    <label class="w3-third">Last name</label>
-    <input class="w3-twothird field input_margin form_readonly" id="last_name" name="last_name" type="text" value="<?php echo $last_name; ?>" required readonly></input>	<br><br>		
-    <label class="w3-third">Email</label>
-    <input class="w3-twothird field input_margin form_readonly" id="email" name="email" type="email" value="<?php echo $email; ?>" required readonly></input><br><br>		
-    <h4>Payment Details</h4>			
-    <label class="w3-third">Reference</label>
-    <input class="w3-twothird field input_margin form_readonly" id="agent_reference" name="agent_reference" type="text" value="<?php echo $reference; ?>" required readonly></input><br><br>
-<!--    <input class="w3-third input_margin form_readonly" id="reference" name="reference" type="hidden" readonly></input>-->
-    <label class="w3-third">Amount Due</label>
-    <input class="w3-twothird field input_margin form_readonly" id="amount_due" name="amount_due" type="text" value="<?php echo $amount_due; ?>" required readonly></input><br><br>
-    <label class="w3-third">Cardholder's Name</label>
-    <input class="w3-twothird field input_margin shadow" id="cardholder" name="cardholder" type="text" data-stripe="name"></input><br><br>
+-->     
+    <div class="w3-row">
+      <label class="w3-third">Title</label>
+      <input class="w3-twothird field input_margin form_readonly" id="title" name="title" type="text" value="<?php echo $title; ?>" style="width:50px" required readonly></input>
+    </div>
+    <div class="w3-row">
+      <label class="w3-third">First name</label>
+      <input class="w3-twothird field input_margin form_readonly" id="first_name" name="first_name" type="text" value="<?php echo $first_name; ?>" required readonly></input>
+    </div>
+    <div class="w3-row">
+      <label class="w3-third">Last name</label>
+      <input class="w3-twothird field input_margin form_readonly" id="last_name" name="last_name" type="text" value="<?php echo $last_name; ?>" required readonly></input>	
+    </div>
+    <div class="w3-row">
+      <label class="w3-third">Email</label>
+      <input class="w3-twothird field input_margin form_readonly" id="email" name="email" type="email" value="<?php echo $email; ?>" required readonly></input><br><br>		
+      <h4>Payment Details</h4>	
+    </div>
+    <div class="w3-row">
+      <label class="w3-third">Reference</label>
+      <input class="w3-twothird field input_margin form_readonly" id="agent_reference" name="agent_reference" type="text" value="<?php echo $reference; ?>" required readonly></input>
+  <!--    <input class="w3-third input_margin form_readonly" id="reference" name="reference" type="hidden" readonly></input>-->
+    </div>
+    <div class="w3-row">
+      <label class="w3-third">Amount Due</label>
+      <input class="w3-twothird field input_margin form_readonly" id="amount_due" name="amount_due" type="text" value="<?php echo $amount_due; ?>" required readonly></input>
+    </div>
+    <div class="w3-row">
+      <label class="w3-third">Cardholder's Name</label>
+      <input class="w3-twothird field input_margin shadow" id="cardholder" name="cardholder" type="text" data-stripe="name"></input>
+    </div>
+    <div class="w3-row">
+      <label class="w3-third" for="card-element">Card Number</label>
+      <div class="w3-twothird">
+        <!--CARD-->
+        <div id="card-element" class="field input_margin shadow"></div> 
+        <input id="submit_button" name="submit_button" type="submit" value="Submit"></input> 	
+        <button id="privacy-policy-link"><a href="https://vmssecurepay.jkamradcliffe.net/index.php/payment_form/privacy_policy">Privacy Policy</a></button>          
+      </div>          
+    </div>
+<!--
+    <div class="w3-row" style="background-color:red">
+      <div class="w3-col m4 l4" style="background-color:green"> </div>
+      <div class="w3-col m8 l8" style="background-color:blue">
+        <input id="submit_button" name="submit_button" type="submit" value="Submit"></input> 	
+        <a href="https://vmssecurepay.jkamradcliffe.net/index.php/payment_form/privacy_policy" id="privacy-policy-link"><p>Privacy Policy</p></a>     
+      </div>
+    </div>
+-->
 
-<!--CARD-->
-
-    <label class="w3-third" for="card-element">Card Number</label>
-    <div id="card-element" class="w3-twothird field input_margin shadow"></div>
 
 
 
